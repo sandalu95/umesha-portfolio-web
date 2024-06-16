43375d71bdd06c9c091c8bb4449180702d92e6c9
sandalu95 <sandalukalpanee@gmail.com>
2024-06-16 15:44:50 +1000

    improve the page transition

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -16,6 +16,15 @@ const StairTransition = () => {
             <Stairs />
           </div>
         </div>
+
+        <motion.div
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+          }}
+        />
       </AnimatePresence>
     </>
   );
