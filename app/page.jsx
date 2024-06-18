ad05aed718d8f7bc20258daff16c5827bd27c606
sandalu95 <sandalukalpanee@gmail.com>
2024-06-18 22:54:36 +1000

    implement cv download

--- app/page.jsx
+++ app/page.jsx
@@ -24,14 +24,16 @@ const Home = () => {
             </p>
             {/* Buttons and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-              >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="/Umesha_Sandalu.pdf" download>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
